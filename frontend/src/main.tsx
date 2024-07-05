928214b6d3a0f5195c551e65dcf3e6b984f92f87
wenshiu.hsu <wenshiu.hsu@gmail.com>
2024-07-05 10:29:31 +0800

    feat(frontend): 🎸 dark mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Root } from "./route";
 import "./index.css";
+import { Root } from "./route";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
--- frontend/src/routes/MainLayout.tsx
+++ frontend/src/routes/MainLayout.tsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router-dom";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
 
 function App() {
     return (
         <>
-            <h1>Scraping.Cal</h1>
-            <Outlet />
+            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                <h1>Scraping.Cal</h1>
+                <ModeToggle />
+                <Outlet />
+            </ThemeProvider>
         </>
     );
 }
